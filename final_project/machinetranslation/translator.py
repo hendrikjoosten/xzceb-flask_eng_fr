97c9e67f39a28ef4348fd339a765268eb8fc2035
hj <joostenhendrik@gmail.com>
2022-09-12 15:33:44 +0000

    translator.py working

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,32 +1,38 @@
-import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
 # set the api from the .env file to authenticate
 authenticator = IAMAuthenticator(apikey)
+
 # declare the language translator object
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator)
-# set the service url from the url in the .env file    
+# set the service url from the url in the .env file
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
-    frenchText = language_translator.translate(
-        text=englishText,
+
+def englishToFrench(english_text):
+    """
+    this function translates a string from frnech to english
+    """
+    french_text = language_translator.translate(
+        text=english_text,
         model_id='en-fr').get_result()
-    return frenchText["translations"][0]["translation"]
+    return french_text["translations"][0]["translation"]
 
 
-def frenchToEnglish(frenchText):
-    englishText = language_translator.translate(
-        text=frenchText,
+def frenchToEnglish(french_text):
+    """
+    this function translates a string from frnech to english
+    """
+    english_text = language_translator.translate(
+        text=french_text,
         model_id='fr-en').get_result()
-    return englishText["translations"][0]["translation"]
-
+    return english_text["translations"][0]["translation"]
